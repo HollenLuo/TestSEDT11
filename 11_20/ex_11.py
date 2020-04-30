92dc01fd8788ffd511b56aa868ab8f431f70f61d
Hollen <luohengbin@yeah.net>
2020-04-30 10:26:37 +0800

    SDET11 Test

---
+++ 11_20/ex_11.py
@@ -0,0 +1,5 @@
+# -*- coding:utf8 -*-
+
+print("How old are you?")
+age = raw_input()
+print("How tall are you?")
---
+++ 1_10/ChapterOne_5.py
@@ -0,0 +1,24 @@
+# -*- coding:utf8 -*-
+
+name = 'Zed A. Shaw'
+age = 35  # not a lie
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hari = 'Brown'
+
+#打印
+
+print("Let's talk about %s." % name)
+print("He's %d inches tall." % height)
+print("He's %d pounds heave." % weight)
+print("Actually that's not too heavy.")
+print("He's got %s eyes and %s hair." %(eyes, hari))
+print("His teeth are usually %s depending on the coffee." % teeth)
+
+# this line is tricky, try to get it exactly right
+print("If I add %d, %d, and %d I get %d." % (age, height, weight, age + height + weight))
+
+
+
